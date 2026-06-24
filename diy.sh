fa0511bf749164d27f6f4f19ad1a4c12f5e0b882
frank0988122 <frank0988@proton.me>
2026-06-24 15:43:32 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -29,14 +29,6 @@ git clone --depth 1 https://github.com/nikkinikki-org/OpenWrt-nikki.git /tmp/nik
 mv /tmp/nikki/luci-app-nikki package/
 mv /tmp/nikki/nikki package/
 
-# 移除 openwrt feeds 自带的核心库
-rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,v2ray-plugin,xray-plugin,geoview,shadow-tls}
-git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
-
-# 移除 openwrt feeds 过时的luci版本
-rm -rf feeds/luci/applications/luci-app-passwall
-git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-luci
-
 
 # 移除旧版daed
 rm -rf feeds/packages/net/dae
